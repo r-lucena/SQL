e8cb8c999cbfeb715972ad1c275593df7ecd1806
Ruben Lucena <rubenlucena89@gmail.com>
2024-07-08 21:25:56 +0200

    Edited sql file

--- SQLite (2).sql
+++ SQLite (2).sql
@@ -1,4 +1,4 @@
-INSERT INTO Libros
+INSERT INTO Books
 (title,author,genre,published_year,isbn,price,rating,stock_count,publisher,page_number)
 VALUES
 ('A Journey to the Center', 'Jules Verne', 'Adventure', 1864, 1234567890123, 12.99, 4.5, 10, 'Verne Publishing', 350),
